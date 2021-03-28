782dbbe7ea8facb016e11b02cfa255f809fa41f1
Mr.Samet <s.burgazoglu@gmail.com>
2021-03-28 12:06:49 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
+/*
+*GNU GENERAL PUBLIC LICENSE V3.0
+*Developer:Samet Burgazoğlu
+*Date:27/03/2021
+*This software read lines from programs.txt. For every line of file create a child process in shell and execute line in shell.
+*/
+
 #include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<string.h>
 #include <sys/wait.h>
+#include <limits.h>
 #define MAX_LINE_WIDTH 128
 
-void free_array(char** array)
+void free_array(char** array)//free a array taken from malloc
 {
     int i;
 
@@ -16,35 +24,34 @@ void free_array(char** array)
 }
 
 int main(void){
-    int fd = open("commands.txt", O_RDONLY);
-    char character;
-    char newline[MAX_LINE_WIDTH];
-    int newline_current_index = 0;
-    int line_counter = 0;
-    char **parameters = malloc(line_counter * sizeof(char*));
+    int fd = open("programs.txt", O_RDONLY);//get file for reading
+    char character;//current character in file
+    char newline[MAX_LINE_WIDTH];//char array for every line
+    int newline_current_index = 0;//current seek in line
+    int line_counter = 0;//how many line read
+    char **parameters = malloc(line_counter * sizeof(char*));//lines from file
 
     while(read(fd, &character, 1) != 0) {
-        newline[newline_current_index] = character;
-        if(character == '\n'){
-            line_counter++;
-            parameters = realloc(parameters, line_counter * sizeof(char*));
-            parameters[line_counter-1] = malloc(newline_current_index*sizeof(char));
-            memcpy(parameters[line_counter-1], newline, newline_current_index);
-            newline_current_index = -1;
-            memset(newline, 0, 128);
+        newline[newline_current_index] = character;//get a character from file
+        if(character == '\n'){//if we are at end of the line
+            line_counter++;//there is a new line
+            parameters = realloc(parameters, line_counter * sizeof(char*));//take space for new line
+            parameters[line_counter-1] = malloc(newline_current_index*sizeof(char));//take space for last line in parameters
+            memcpy(parameters[line_counter-1], newline, newline_current_index);//copy current line to parameters last value
+            newline_current_index = -1;//this will be 0 in start of line
+            memset(newline, 0, 128);//reset char array
             }
-        newline_current_index++;
+        newline_current_index++;//new character
         }
-    for(int i = 0; i < line_counter; ++i){
-        printf("%s\n", parameters[i]);
-        int a = fork();
-        if(fork() == 0){
-            char *args[] = {"/bin/sh", "-c", parameters[i], NULL};
-            execve("/bin/sh", args, NULL);
-            exit(0);
+    for(int i = 0; i < line_counter; ++i){//every line of file
+        int a = fork();//create child process
+        if(fork() == 0){//if current process is child process
+            char *args[] = {"/bin/sh", "-c", parameters[i], NULL};//args for shell
+            execve("/bin/sh", args, NULL);//give line to shell for execute
+            exit(0);//exit
         }
     }
-    close(fd);
-    free_array(parameters);
+    free_array(parameters);//give space back
+    close(fd);//close file
     return 0;
 }
